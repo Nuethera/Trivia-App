29c75ca8dd2b672479a12817f0b8eab143bc409c
Nuethera <vaishnavj2124@outlook.com>
2022-10-16 18:51:25 +0530

    cleaning code

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ for question in qb.question_data:
 
 quiz = QuizBrain(question_bank)
 ui = QuizInterface(quiz)
-# while quiz.still_has_questions():
-#     quiz.next_question()
 
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
